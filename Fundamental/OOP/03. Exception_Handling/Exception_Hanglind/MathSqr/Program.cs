9f7fecebcc3793f251f2ec273ab064aa3f6ff995
Alexander Mitev <alexander.mitev@outlook.com>
2016-05-08 20:40:18 +0300

    Added a new Handling_Exception project that will cover the topic

---
+++ Fundamental/OOP/03. Exception_Handling/Exception_Hanglind/MathSqr/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSqr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            try
+            {
+                
+                int num = int.Parse(Console.ReadLine());
+                if(num < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (ArgumentNullException )
+            {
+                Console.WriteLine("Number cannot be null");
+            }
+            catch (ArgumentOutOfRangeException )
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch   (ArgumentException )
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch(Exception )
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
